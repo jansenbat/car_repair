236bda8f84c65cac26657d12482e5cb9608498db
Jansen Rodrigo <103814467+jansenbat@users.noreply.github.com>
2022-09-04 07:39:35 -1000

    Simple info

---
+++ car_pay.py
@@ -0,0 +1,12 @@
+"""
+Have to pay a large amount of money to fix my car (blown head gasket, etc.)
+- Luckily I can pay it off monthly after putting some money down
+- This program is to help me keep track of it
+- Help me get acquainted with SQL (sqlite3)
+"""
+
+# Enter the full amount of the repair bill
+# Add date, amount paid, and note the amount left
+
+# import sql
+# write a few variables and functions
